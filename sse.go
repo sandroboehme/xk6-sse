b33d00f4befb2ea1cf1f7df7f7ae4143f5661709
Sandro Böhme <sandro.boehme@inovex.de>
2025-02-20 14:11:46 +0100

    changed event handling

--- sse.go
+++ sse.go
@@ -389,7 +389,7 @@ func (c *Client) readEvents(readChan chan Event, errorChan chan error, closeChan
 			}
 		}
 
-		fmt.Printf("Received line: |%s|\n", string(line))
+		fmt.Printf("Received line: =>%s<=\n", string(line))
 		//fmt.Println("Received line:", string(line)) // Log received line
 
 		// Append line to buffer
