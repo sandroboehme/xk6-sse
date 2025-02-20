3aff02e185d4d199c8f92b8150d959bf3c63dfbb
Sandro Böhme <sandro.boehme@inovex.de>
2025-02-20 09:37:39 +0100

    changed event handling

--- sse.go
+++ sse.go
@@ -1,37 +1,24 @@
-// Package sse implements a k6/x/sse javascript module extension for k6.
-// It provides basic functionality to handle Server-Sent Event over http
-// that *blocks* the event loop while the http connection is opened.
-// [SSE API design document]:
-// https://github.com/phymbert/xk6-sse/blob/master/docs/design/021-sse-api.md#proposed-solution
 package sse
 
 import (
 	"bufio"
 	"bytes"
 	"context"
-	"crypto/tls"
 	"errors"
 	"fmt"
 	"io"
-	"net"
 	"net/http"
-	"net/http/cookiejar"
-	"net/http/httptrace"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/grafana/sobek"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
-	httpModule "go.k6.io/k6/js/modules/k6/http"
 	"go.k6.io/k6/lib"
 	"go.k6.io/k6/metrics"
 )
 
 type (
-	// sse represents a module instance of the sse module.
 	sse struct {
 		vu      modules.VU
 		obj     *sobek.Object
@@ -39,10 +26,8 @@ type (
 	}
 )
 
-// ErrSSEInInitContext is returned when sse are using in the init context
 var ErrSSEInInitContext = common.NewInitContextError("using sse in the init context is not supported")
 
-// Client is the representation of the sse returned to the js.
 type Client struct {
 	rt            *sobek.Runtime
 	ctx           context.Context
@@ -59,7 +44,6 @@ type Client struct {
 	cancelRequest  context.CancelFunc
 }
 
-// HTTPResponse is the http response returned by sse.open.
 type HTTPResponse struct {
 	URL     string            `json:"url"`
 	Status  int               `json:"status"`
@@ -67,7 +51,6 @@ type HTTPResponse struct {
 	Error   string            `json:"error"`
 }
 
-// Event represents a Server-Sent Event
 type Event struct {
 	ID      string
 	Comment string
@@ -84,12 +67,10 @@ type sseOpenArgs struct {
 	tagsAndMeta *metrics.TagsAndMeta
 }
 
-// Exports returns the exports of the sse module.
 func (mi *sse) Exports() modules.Exports {
 	return modules.Exports{Default: mi.obj}
 }
 
-// Open establishes a http client connection based on the parameters provided.
 func (mi *sse) Open(url string, args ...sobek.Value) (*HTTPResponse, error) {
 	ctx := mi.vu.Context()
 	rt := mi.vu.Runtime()
@@ -108,7 +89,6 @@ func (mi *sse) Open(url string, args ...sobek.Value) (*HTTPResponse, error) {
 	client, connEndHook, err := mi.open(ctx, state, rt, url, parsedArgs)
 	defer connEndHook()
 	if err != nil {
-		// Pass the error to the user script before exiting immediately
 		client.handleEvent("error", rt.ToValue(err))
 		if state.Options.Throw.Bool {
 			return nil, err
@@ -116,27 +96,23 @@ func (mi *sse) Open(url string, args ...sobek.Value) (*HTTPResponse, error) {
 		return client.wrapHTTPResponse(err.Error()), nil
 	}
 
-	// Run the user-provided set up function
 	if _, err := parsedArgs.setupFn(sobek.Undefined(), rt.ToValue(&client)); err != nil {
 		_ = client.closeResponseBody()
 		return nil, err
 	}
 
-	// The connection is now open, emit the event
 	client.handleEvent("open")
 
-	readEventChan := make(chan Event)
-	readErrChan := make(chan error)
-	readCloseChan := make(chan int)
+	readEventChan := make(chan Event, 1)  // Buffered channel to avoid blocking
+	readErrChan := make(chan error, 1)
+	readCloseChan := make(chan int, 1)
 
-	// Wraps a couple of channels
 	go client.readEvents(readEventChan, readErrChan, readCloseChan)
 
-	// This is the main control loop. All JS code (including error handlers)
-	// should only be executed by this thread to avoid race conditions
 	for {
 		select {
 		case event := <-readEventChan:
+			fmt.Println("Event received:", event) // Ensure event is received
 			metrics.PushIfNotDone(ctx, client.samplesOutput, metrics.Sample{
 				TimeSeries: metrics.TimeSeries{
 					Metric: client.sseMetrics.SSEEventReceived,
@@ -153,117 +129,26 @@ func (mi *sse) Open(url string, args ...sobek.Value) (*HTTPResponse, error) {
 			client.handleEvent("error", rt.ToValue(readErr))
 
 		case <-ctx.Done():
-			// VU is shutting down during an interrupt
-			// client events will not be forwarded to the VU
 			_ = client.closeResponseBody()
 
 		case <-readCloseChan:
 			_ = client.closeResponseBody()
 
 		case <-client.done:
-			// This is the final exit point normally triggered by closeResponseBody
 			return client.wrapHTTPResponse(""), nil
 		}
 	}
 }
 
-func (mi *sse) open(ctx context.Context, state *lib.State, rt *sobek.Runtime,
-	url string, args *sseOpenArgs,
-) (*Client, func(), error) {
-	reqCtx, cancel := context.WithCancel(ctx)
-
-	sseClient := Client{
-		ctx:            ctx,
-		rt:             rt,
-		url:            url,
-		eventHandlers:  make(map[string][]sobek.Callable),
-		done:           make(chan struct{}),
-		samplesOutput:  state.Samples,
-		tagsAndMeta:    args.tagsAndMeta,
-		builtinMetrics: state.BuiltinMetrics,
-		sseMetrics:     mi.metrics,
-		cancelRequest:  cancel,
-	}
-
-	// Overriding the NextProtos to avoid talking http2
-	var tlsConfig *tls.Config
-	if state.TLSConfig != nil {
-		tlsConfig = state.TLSConfig.Clone()
-		tlsConfig.NextProtos = []string{"http/1.1"}
-	}
-
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			DialContext:     state.Dialer.DialContext,
-			Proxy:           http.ProxyFromEnvironment,
-			TLSClientConfig: tlsConfig,
-			// FIXME phymbert: it would be more interesting to allow reusing the transport across iterations
-			DisableKeepAlives: state.Options.NoConnectionReuse.ValueOrZero() || state.Options.NoVUConnectionReuse.ValueOrZero(),
-		},
-	}
-
-	// httpClient.Jar must never be nil
-	if args.cookieJar != nil {
-		httpClient.Jar = args.cookieJar
-	}
-
-	httpMethod := http.MethodGet
-	if args.method != "" {
-		httpMethod = args.method
-	}
-
-	req, err := http.NewRequestWithContext(reqCtx, httpMethod, url, strings.NewReader(args.body))
-	if err != nil {
-		return &sseClient, nil, err
-	}
-
-	req.Header.Set("Accept", "text/event-stream")
-	for headerName, headerValues := range args.headers {
-		for _, headerValue := range headerValues {
-			req.Header.Set(headerName, headerValue)
-		}
-	}
-
-	// Wrap the request to retrieve the server IP tag
-	trace := &httptrace.ClientTrace{
-		GotConn: func(connInfo httptrace.GotConnInfo) {
-			if state.Options.SystemTags.Has(metrics.TagIP) {
-				if ip, _, err2 := net.SplitHostPort(connInfo.Conn.RemoteAddr().String()); err2 == nil {
-					args.tagsAndMeta.SetSystemTagOrMeta(metrics.TagIP, ip)
-				}
-			}
-		},
-	}
-
-	//nolint:contextcheck // parent context already passed in the request
-	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
-
-	connStart := time.Now()
-	//nolint:bodyclose // Body is deferred closed in closeResponseBody
-	resp, err := httpClient.Do(req)
-	connEnd := time.Now()
-
-	if resp != nil {
-		sseClient.resp = resp
-		if state.Options.SystemTags.Has(metrics.TagStatus) {
-			args.tagsAndMeta.SetSystemTagOrMeta(
-				metrics.TagStatus, strconv.Itoa(resp.StatusCode))
-		}
-	}
-
-	connEndHook := sseClient.pushSSEMetrics(connStart, connEnd)
-
-	return &sseClient, connEndHook, err
-}
-
-// On is used to configure what the client should do on each event.
 func (c *Client) On(event string, handler sobek.Value) {
 	if handler, ok := sobek.AssertFunction(handler); ok {
 		c.eventHandlers[event] = append(c.eventHandlers[event], handler)
+		fmt.Printf("Registered handler for event: %s\n", event) // Log registration
+	} else {
+		fmt.Printf("Failed to register handler for event: %s\n", event)
 	}
 }
 
-// Close the event loop
 func (c *Client) Close() error {
 	err := c.closeResponseBody()
 	c.cancelRequest()
@@ -271,92 +156,20 @@ func (c *Client) Close() error {
 }
 
 func (c *Client) handleEvent(event string, args ...sobek.Value) {
+	fmt.Printf("Handling event: %s\n", event) // Log event handling
 	if handlers, ok := c.eventHandlers[event]; ok {
 		for _, handler := range handlers {
+			fmt.Printf("Invoking handler for event: %s\n", event) // Log handler invocation
 			if _, err := handler(sobek.Undefined(), args...); err != nil {
+				fmt.Printf("Error in handler for event: %s, error: %v\n", event, err)
 				common.Throw(c.rt, err)
 			}
 		}
+	} else {
+		fmt.Printf("No handlers registered for event: %s\n", event)
 	}
 }
 
-// closeResponseBody cleanly closes the response body.
-// Returns an error if sending the response body cannot be closed.
-func (c *Client) closeResponseBody() error {
-	var err error
-
-	c.shutdownOnce.Do(func() {
-		err = c.resp.Body.Close()
-		if err != nil {
-			c.handleEvent("error", c.rt.ToValue(err))
-		}
-		close(c.done)
-	})
-
-	return err
-}
-
-func (c *Client) pushSSEMetrics(connStart, connEnd time.Time) func() {
-	connDuration := metrics.D(connEnd.Sub(connStart))
-
-	metrics.PushIfNotDone(c.ctx, c.samplesOutput, metrics.ConnectedSamples{
-		Samples: []metrics.Sample{
-			{
-				TimeSeries: metrics.TimeSeries{
-					Metric: c.builtinMetrics.HTTPReqSending,
-					Tags:   c.tagsAndMeta.Tags,
-				},
-				Time:     connStart,
-				Metadata: c.tagsAndMeta.Metadata,
-				Value:    connDuration,
-			},
-		},
-		Tags: c.tagsAndMeta.Tags,
-		Time: connStart,
-	})
-
-	return func() {
-		end := time.Now()
-		requestDuration := metrics.D(end.Sub(connStart))
-
-		metrics.PushIfNotDone(c.ctx, c.samplesOutput, metrics.ConnectedSamples{
-			Samples: []metrics.Sample{
-				{
-					TimeSeries: metrics.TimeSeries{
-						Metric: c.builtinMetrics.HTTPReqs,
-						Tags:   c.tagsAndMeta.Tags,
-					},
-					Time:     end,
-					Metadata: c.tagsAndMeta.Metadata,
-					Value:    1,
-				},
-				{
-					TimeSeries: metrics.TimeSeries{
-						Metric: c.builtinMetrics.HTTPReqSending,
-						Tags:   c.tagsAndMeta.Tags,
-					},
-					Time:     end,
-					Metadata: c.tagsAndMeta.Metadata,
-					Value:    connDuration,
-				},
-				{
-					TimeSeries: metrics.TimeSeries{
-						Metric: c.builtinMetrics.HTTPReqDuration,
-						Tags:   c.tagsAndMeta.Tags,
-					},
-					Time:     end,
-					Metadata: c.tagsAndMeta.Metadata,
-					Value:    requestDuration,
-				},
-			},
-			Tags: c.tagsAndMeta.Tags,
-			Time: end,
-		})
-	}
-}
-
-// Wraps SSE in a channel, follow the SSE format described in:
-// https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
 func (c *Client) readEvents(readChan chan Event, errorChan chan error, closeChan chan int) {
 	reader := bufio.NewReader(c.resp.Body)
 	ev := Event{}
@@ -368,123 +181,56 @@ func (c *Client) readEvents(readChan chan Event, errorChan chan error, closeChan
 			if errors.Is(err, io.EOF) {
 				fmt.Println("EOF reached; closing connection.")
 				select {
-					case closeChan <- -1:
-						return
-					case <-c.done:
-						return
+				case closeChan <- -1:
+					return
+				case <-c.done:
+					return
 				}
 			} else {
 				fmt.Printf("Error reading line: %v\n", err)
 				select {
-					case errorChan <- err:
-						return
-					case <-c.done:
-						return
+				case errorChan <- err:
+					return
+				case <-c.done:
+					return
 				}
 			}
 		}
 
-		fmt.Println("Received line:", string(line))  // Log received line
+		fmt.Println("Received line:", string(line)) // Log received line
 
 		switch {
-		// Handle event parsing...
+		case hasPrefix(line, "id: "):
+			ev.ID = stripPrefix(line, 4)
+			fmt.Println("Parsed ID:", ev.ID)
+		case hasPrefix(line, "event: "):
+			ev.Name = stripPrefix(line, 7)
+			fmt.Println("Parsed Event Name:", ev.Name)
+		case hasPrefix(line, "data: "):
+			buf.Write(line[6:])
+			fmt.Println("Appending Data:", string(line[6:]))
+		case bytes.Equal(line, []byte("\n")):
+			ev.Data = strings.TrimRightFunc(buf.String(), func(r rune) bool {
+				return r == '\r' || r == '\n'
+			})
+
+			fmt.Printf("Complete Event: ID=%s, Name=%s, Data=%s\n", ev.ID, ev.Name, ev.Data)
+
+			select {
+			case readChan <- ev:
+				fmt.Println("Event sent to channel")
+				buf.Reset()
+				ev = Event{}
+			case <-c.done:
+				return
+			}
+		default:
+			fmt.Println("Unknown line format:", string(line))
+			select {
+			case errorChan <- errors.New("unknown event: " + string(line)):
+			case <-c.done:
+				return
+			}
 		}
 	}
-}
-
-// Wrap the raw HTTPResponse we received to a sse.HTTPResponse we can pass to the user
-func (c *Client) wrapHTTPResponse(errMessage string) *HTTPResponse {
-	if errMessage != "" {
-		return &HTTPResponse{Error: errMessage}
-	}
-	sseResponse := HTTPResponse{
-		URL:    c.url,
-		Status: c.resp.StatusCode,
-	}
-
-	sseResponse.Headers = make(map[string]string, len(c.resp.Header))
-	for k, vs := range c.resp.Header {
-		sseResponse.Headers[k] = strings.Join(vs, ", ")
-	}
-
-	return &sseResponse
-}
-
-func parseConnectArgs(state *lib.State, rt *sobek.Runtime, args ...sobek.Value) (*sseOpenArgs, error) {
-	// The params argument is optional
-	var callableV, paramsV sobek.Value
-	switch len(args) {
-	case 2:
-		paramsV = args[0]
-		callableV = args[1]
-	case 1:
-		paramsV = sobek.Undefined()
-		callableV = args[0]
-	default:
-		return nil, errors.New("invalid number of arguments to sse.open")
-	}
-	// Get the callable (required)
-	setupFn, isFunc := sobek.AssertFunction(callableV)
-	if !isFunc {
-		return nil, errors.New("last argument to sse.open must be a function")
-	}
-
-	headers := make(http.Header)
-	headers.Set("User-Agent", state.Options.UserAgent.String)
-	tagsAndMeta := state.Tags.GetCurrentValues()
-	parsedArgs := &sseOpenArgs{
-		setupFn:     setupFn,
-		headers:     headers,
-		cookieJar:   state.CookieJar,
-		tagsAndMeta: &tagsAndMeta,
-	}
-
-	if sobek.IsUndefined(paramsV) || sobek.IsNull(paramsV) {
-		return parsedArgs, nil
-	}
-
-	// Parse the optional second argument (params)
-	params := paramsV.ToObject(rt)
-	for _, k := range params.Keys() {
-		switch k {
-		case "headers":
-			headersV := params.Get(k)
-			if sobek.IsUndefined(headersV) || sobek.IsNull(headersV) {
-				continue
-			}
-			headersObj := headersV.ToObject(rt)
-			if headersObj == nil {
-				continue
-			}
-			for _, key := range headersObj.Keys() {
-				parsedArgs.headers.Set(key, headersObj.Get(key).String())
-			}
-		case "tags":
-			if err := common.ApplyCustomUserTags(rt, parsedArgs.tagsAndMeta, params.Get(k)); err != nil {
-				return nil, fmt.Errorf("invalid sse.open() metric tags: %w", err)
-			}
-		case "jar":
-			jarV := params.Get(k)
-			if sobek.IsUndefined(jarV) || sobek.IsNull(jarV) {
-				continue
-			}
-			if v, ok := jarV.Export().(*httpModule.CookieJar); ok {
-				parsedArgs.cookieJar = v.Jar
-			}
-		case "method":
-			parsedArgs.method = strings.TrimSpace(params.Get(k).ToString().String())
-		case "body":
-			parsedArgs.body = strings.TrimSpace(params.Get(k).ToString().String())
-		}
-	}
-
-	return parsedArgs, nil
-}
-
-func hasPrefix(s []byte, prefix string) bool {
-	return bytes.HasPrefix(s, []byte(prefix))
-}
-
-func stripPrefix(line []byte, start int) string {
-	return string(line[start : len(line)-1])
 }
